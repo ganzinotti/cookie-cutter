0ffe91f337fdb6f7f9e6b08cd6e72af4bfc6a944
Giovanni Ganzinotti <Giovanni.Ganzinotti@klm.com>
2020-02-28 17:02:03 +0100

    retry

--- project/brain.py
+++ project/brain.py
@@ -1,3 +1,7 @@
+import numpy as np
+from project.other_module import constant
+
+
 def add_number(a, b):
     """
     This is a very complex function
@@ -7,3 +11,7 @@ def add_number(a, b):
     :return: int
     """
     return a + b
+
+
+def multiple_number(a, b):
+    return np.array(constant * b)
